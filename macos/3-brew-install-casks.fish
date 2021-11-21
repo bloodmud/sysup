dd4b9244b12c5deaf9dd854dcbcadc13c75da773
bloodmud <bloodmud@gmail.com>
2021-11-21 19:32:50 +0800

    update cask intall ...

--- macos/3-brew-install-casks.fish
+++ macos/3-brew-install-casks.fish
@@ -1,38 +1,39 @@
 #! /usr/local/bin/fish
 #
 
-brew cask install alfred
-brew cask install moom
-brew cask install iterm2
+brew install keka    # archive tool
+brew install alfred
+brew install moom
+brew install iterm2
 
-#brew cask install go2shell
-brew cask install dupeguru
+#brew install go2shell
+#brew install dupeguru
 
 # Development
-brew cask install meld
+brew install meld
 
-brew cask install gimp
+brew install gimp
 # pdf editor
-brew cask install skim
+brew install skim
 # markdown editor
-brew cask install typora
+brew install typora
 # ebook management & converter
-brew cask install calibre
+brew install calibre
 
-brew cask install textmate
-#brew cask install sublime-text
+brew install textmate
+#brew install sublime-text
 
-brew cask install docker
+brew install docker
 
-brew cask install firefox
-brew cask install google-chrome
+brew install firefox
+brew install google-chrome
 
 # Media
-brew cask install vlc
-brew cask install neteasemusic
+brew install vlc
+brew install neteasemusic
 
 # demo recorder
-#brew cask install licecap
+#brew install licecap
 
 # audio edit
-brew cask install ocenaudio
+brew install ocenaudio
