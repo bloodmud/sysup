090a2d9e8f022850fe6116bd9059ed65f6a3dd0b
bloodmud <bloodmud@gmail.com>
2022-05-03 18:30:29 +0800

    update linix kernel

--- stretch/upgrade_all.sh
+++ stretch/upgrade_all.sh
@@ -9,3 +9,12 @@ apt-get upgrade
 
 apt-get install linux-image-${ARCHINAME}
 apt-get install linux-headers-${ARCHINAME}
+
+echo "#############################################"
+echo "#                                           #"
+echo "#   After restart, run:                     #"
+echo "#   apt-get autoremove                      #"
+echo "#   apt-get autoclean                       #"
+echo "#                                           #"
+echo "#############################################"
+
